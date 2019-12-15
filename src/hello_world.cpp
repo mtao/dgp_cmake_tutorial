6f498f79f295d174367c07ecf0e0053b90daf98c
Michael Tao <mtao@dgp.toronto.edu>
2019-12-15 13:21:37 -0500

    added boost::filesystem in order to show how to use a dependency

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <boost/filesystem.hpp>
+
+std::string get_dir(const std::string& str) {
+    using namespace boost::filesystem;
+    path p(str);
+    if(exists(p)) {
+        return canonical(p.parent_path()).string();
+    } else {
+        return "UNKNOWN?";
+    }
+
+}
 
 int main(int argc, char * argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    std::string dir = get_dir(argv[0]);
+    std::cout << "From [" << dir << "]: Hello world!" << std::endl;
     return 0;
 }
 
