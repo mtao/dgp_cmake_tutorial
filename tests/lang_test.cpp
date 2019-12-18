709fe9ed0a60322d356609e566619a2e1a6ef59f
Michael Tao <mtao@dgp.toronto.edu>
2019-12-18 10:30:50 -0500

    objects work

--- tests/lang_test.cpp
+++ tests/lang_test.cpp
@@ -1,11 +1,9 @@
-#define CATCH_CONFIG_MAIN
-
 #include <catch2/catch.hpp>
 #include "localization.hpp"
 
 
 
-TEST_CASE( "Languages are included", LANG_TEST_LANG )
+TEST_CASE( "lang_test_" LANG_TEST_LANG, LANG_TEST_LANG )
 {
     REQUIRE( get_hello( LANG_TEST_LANG ).size() > 0 );
 }
